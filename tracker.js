0a16246ca13a569b299e8c5ba1620081d13be49c
Austin Hillyard <austin.hillyard@gmail.com>
2023-10-25 23:00:45 -0600

    Added JS to import tool and Tracker. Import tool had some issues with APIs and reading JSON string to read a API KEY. In short, the importing of data via an API will need to be done server side. For now my JSON data is included as a default. Import tool does add that JSON data to local storage, and grabs users steamID from the provided URL. Tracker tool has some functions outlined, along with the Game Class started. It also loads the loadGames function when the page loads. Obviously still a WIP.

--- tracker.js
+++ tracker.js
@@ -13,6 +13,33 @@ Click on a game and have it include a popup view that shows more details
 Introduce sorting and filter options.
 
 */
+const games = [];
+
+class Game {
+    constructor(gameJson) {
+        this.name = gameJson.name;
+        this.playtime = gameJson.playtime_forever;
+        this.lastPlayed = gameJson.rtime_last_played;
+    }
+}
+
+window.onload = function() {
+    const gamesString = localStorage.getItem("games");
+    if (gamesString != null) {
+        loadGames(gamesString);
+    }
+
+    else {
+        document.querySelector("#GameView").innerHTML = "No games detected for user. Please use the import tool";
+    }
+}
+
+async function loadGames(gamesString) {
+    let gamesJson = JSON.parse(gamesString);
+    for (let game in gamesJson.response.games) {
+        games.push(new Game(game));
+    }
+}
 
 //Inserts the game element into the appropriate div 
 function insertGameCard(game) {
