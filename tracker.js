15a432bab6fc409c21d1736979818b13eed93f0b
Austin Hillyard <austin.hillyard@gmail.com>
2023-10-26 22:25:41 -0600

    Added functionality that dynamically gets game data from JSON string, puts them into classes, and then sorts them by last played, and then inserts them into the correct fields. It then creates divs of the games with the relevant data inside of it (name, time played, last played). Still need to add image API, and likely rework how the CSS displays the info.

--- tracker.js
+++ tracker.js
@@ -13,16 +13,42 @@ Click on a game and have it include a popup view that shows more details
 Introduce sorting and filter options.
 
 */
-const games = [];
 
+const now = Date.now();
+
+//Arrays of total games, and games sorted by last play time
+const games = [];
+const oneWeekGames = [];
+const oneMonthGames = [];
+const threeMonthGames = [];
+const sixMonthGames = [];
+const oneYearGames = [];
+const moreThanYearGames = [];
+const neverPlayed = [];
+
+//Class object that games are converted to from JSON string.
 class Game {
     constructor(gameJson) {
         this.name = gameJson.name;
         this.playtime = gameJson.playtime_forever;
-        this.lastPlayed = gameJson.rtime_last_played;
+        this.lastPlayed = this.getTimeFrame(gameJson.rtime_last_played);
+        this.gameID = gameJson.appid;
+        this.epochTime = gameJson.rtime_last_played;
+    }
+
+    getTimeFrame(lastPlayed) {
+        if (lastPlayed != 0) {
+            let date = new Date(0);
+            date.setUTCSeconds(lastPlayed);
+            return date;
+        }
+        else {
+            return "Never Played";
+        }
     }
 }
 
+//Loads the page with games, or a message saying to use the import tool.
 window.onload = function() {
     const gamesString = localStorage.getItem("games");
     if (gamesString != null) {
@@ -36,14 +62,107 @@ window.onload = function() {
 
 async function loadGames(gamesString) {
     let gamesJson = JSON.parse(gamesString);
-    for (let game in gamesJson.response.games) {
+    for (let game of gamesJson.response.games) {
         games.push(new Game(game));
+    }
+    putGamesInTimeSlots(games);
+    insertGames();
+
+}
+
+function putGamesInTimeSlots(games) {
+    for (let game of games) {
+
+        //Test if game never played first
+        if (game.lastPlayed === "Never Played") {
+            neverPlayed.push(game);
+            continue;
+        }
+
+        //Find time difference between last played and now
+        let timeDifference = (now.valueOf() - game.lastPlayed.valueOf()) / 1000;
+        
+        //One week difference
+        if (timeDifference <= 604800) {
+            oneWeekGames.push(game);
+        }
+        //One Month Difference
+        else if (timeDifference <= 2629800) {
+            oneMonthGames.push(game);
+        }
+        //Three Month Difference
+        else if (timeDifference <= 7889400) {
+            threeMonthGames.push(game);
+        }
+        //Six Month Difference
+        else if (timeDifference <= 15778800) {
+            sixMonthGames.push(game);
+        }
+        //One Year Difference
+        else if (timeDifference <= 31557600) {
+            oneYearGames.push(game);
+        }
+        //More than 1 Year Difference
+        else {
+            moreThanYearGames.push(game);
+        }
+    }
+}
+
+function insertGames() {
+    for (let game of oneWeekGames) {
+        insertGameCard(game, "lessThanWeek");
+    }
+    for (let game of oneMonthGames) {
+        insertGameCard(game, "lessThan1Month");
+    }
+    for (let game of threeMonthGames) {
+        insertGameCard(game, "lessThan3Months");
+    }
+    for (let game of sixMonthGames) {
+        insertGameCard(game, "lessThan6Months");
+    }
+    for (let game of oneYearGames) {
+        insertGameCard(game, "lessThanYear");
+    }
+    for (let game of moreThanYearGames) {
+        insertGameCard(game, "moreThanYear");
+    }
+    for (let game of neverPlayed) {
+        insertGameCard(game, "neverPlayed");
     }
 }
 
 //Inserts the game element into the appropriate div 
-function insertGameCard(game) {
-    
+function insertGameCard(game, section) {
+    let gameCard = document.createElement("div");
+
+    let gameName = document.createElement("p");
+    let gameTime = document.createElement("p");
+    let gameLastPlayed = document.createElement("p");
+
+    gameName.innerHTML = game.name;
+    gameTime.innerHTML = "Time Played: " + Math.floor(game.playtime / 60) + "hr " + game.playtime % 60 + "min";
+    if (game.lastPlayed === "Never Played") {
+        gameLastPlayed.innerHTML = "Never Played";
+    }
+    else {
+        gameLastPlayed.innerHTML = "Last Played: " + game.lastPlayed.toDateString();
+    }
+
+    gameCard.classList.add("game")
+    gameCard.appendChild(gameName);
+    gameCard.appendChild(gameTime);
+    gameCard.appendChild(gameLastPlayed);
+
+    let parentElement = document.getElementById(section);
+    parentElement.appendChild(gameCard);
+}
+
+//Grab Image from steamGridDB
+//This might have to be done from the Server side. Or I have the key public.
+function steamGridImage(gameID) {
+
 }
 
 //Refresh button that reruns the import functionality
